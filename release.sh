31e132a458427e140da88acce06bc96cd93141ed
Remi Caput <r.caput@criteo.com>
2015-09-26 16:35:10 +0200

    Prepare for folder change in configuration-ui repository.

--- release.sh
+++ release.sh
@@ -2,14 +2,13 @@
 
 deploy()
 {
-	local from_deploy="$3"
-	local from_root="$2"
+	local command="$5"
+	local host="$3"
 	local out=$(mktemp)
 	local repo="$1"
+	local source="$2"
 	local status
-	local to_cmd="$6"
-	local to_host="$4"
-	local to_path="$5"
+	local target="$4"
 
 	(
 		cd "$repo" &&
@@ -19,8 +18,8 @@ deploy()
 			git reset -q --hard origin/master &&
 			cd "$from_root" &&
 			( test '!' -f setup.sh || sh setup.sh > /dev/null ) &&
-			tar cCO "$from_deploy" . |
-			ssh -q "$to_host" "tar xC '$to_path' -f - && ${to_cmd:-true}"
+			tar cCO "$source" . |
+			ssh -q "$host" "tar xC '$target' -f - && ${command:-true}"
 		)
 	) > "$out" 2>&1
 
@@ -35,13 +34,12 @@ deploy()
 	echo "$repo: $status"
 }
 
-#	deploy <repo>			<from_root>			<from_deploy> <to_host>		<to_path>	<to_cmd>
+#	deploy <repo>			<src>	<host>		<target>	<command>
 (
-	deploy api				.					src		nginx@bidtorrent.io	/var/www/io.bidtorrent.api &&
-	deploy bidder-criteo	.					src		nginx@bidtorrent.io	/var/www/io.bidtorrent.test &&
-	deploy client-js		.					build	nginx@bidtorrent.io	/var/www/io.bidtorrent &&
-	deploy configuration-ui	configuration-ui	bin		nginx@bidtorrent.io	/var/www/io.bidtorrent.www &&
-	deploy server			.					nginx	root@bidtorrent.io	/etc/nginx	'service nginx reload'
-	deploy track			.					src		nginx@bidtorrent.io	/var/www/io.bidtorrent.log
+	deploy api				src		nginx@bidtorrent.io	/var/www/io.bidtorrent.api &&
+	deploy bidder-criteo	src		nginx@bidtorrent.io	/var/www/io.bidtorrent.test &&
+	deploy client-js		build	nginx@bidtorrent.io	/var/www/io.bidtorrent &&
+	deploy configuration-ui	bin		nginx@bidtorrent.io	/var/www/io.bidtorrent.www &&
+	deploy server			nginx	root@bidtorrent.io	/etc/nginx	'service nginx reload'
 ) |
 ssh -q www-data@sandstone 'cat > www/io.bidtorrent.doc/foab.txt'
